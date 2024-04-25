6a51024c56f5a447db07f0a092aa67416cc631a9
Daniel Taylor <44615234+djtaylor03@users.noreply.github.com>
2024-04-25 18:46:04 +0100

    Create generate_bart.sh

---
+++ baselines/generate_bart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --nodes=1 
+#SBATCH --gres=gpu:4
+#SBATCH --mem=334GB  
+#SBATCH --mail-user=tgoldsack1@sheffield.ac.uk
+#SBATCH --mail-type=ALL
+
+
+module load Anaconda3/2022.05
+module load CUDA/11.8.0 
+
+source activate NLP
+
+python train_table2text_bart.py \
+--data_dir=dataset\train\few-shot\train.json \
+--model_name_or_path=bart-large \
+--learning_rate=3e-5 \
+--num_train_epochs 30 \
+--train_batch_size=8 \
+--eval_batch_size=4 \
+--test_batch_size=4 \
+--output_dir=outputs\bart_outs \
+--n_gpu 1 \
+--do_train \
+--do_predict \
+--early_stopping_patience 10 \
+--max_source_length 384 \
+--max_target_length 384 \
